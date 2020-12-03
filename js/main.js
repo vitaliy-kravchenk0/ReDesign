087ea39aeb1a608474cfdf5870e2182c7a691906
Vitaliy <vitaliy.blogger@gmail.com>
2020-12-03 18:06:32 +0200

    Начал делать бургер меню

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    // Scroll
+
+
     function scroll(from) {
         $(from).on("click", function (event) {
             event.preventDefault();
@@ -14,4 +17,16 @@ $(document).ready(function(){
     scroll(".list-menu__item-gallery");
     scroll(".list-menu__item-contacts");
     scroll(".training-utp__button");
+
+
+    // Burger
+
+
+    $(".burger-menu").on("click", function(e) {
+        e.preventDefault();
+        $(this).toggleClass("burger-menu_active");
+        document.body.style.overflow = "hidden";
+        $(".burger-modal").toggleClass("show");
+        $(".burger-modal").toggleClass("hide");
+    });
 });
