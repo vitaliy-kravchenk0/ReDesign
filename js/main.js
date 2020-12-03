203a43037652e94f7663c10912e07af84bc13e4a
Vitaliy <vitaliy.blogger@gmail.com>
2020-12-03 11:43:09 +0200

    Сделал плавный scroll, осталось бургер меню и смена языков

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".training-utp__button").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('#contacts'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
